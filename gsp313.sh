74baecd8bb3628247858b11037ecb160fa683a6d
Rafal <rafal.janik@example.com>
2024-07-06 13:41:02 +0200

    GSP313

--- gsp313.sh
+++ gsp313.sh
@@ -1,3 +1,12 @@
+export ZONE=us-west1-c
+export INSTANCE_NAME=nucleus-jumphost-582
+export MACHINE_TYPE=e2-micro
+
+gcloud config set compute/zone $ZONE
+gcloud compute instances create $INSTANCE_NAME \
+--machine-type $MACHINE_TYPE \
+--zone $ZONE
+
 export MACHINE_TYPE=e2-medium
 export REGION=us-west4
 export ZONE=us-west4-a
@@ -6,66 +15,65 @@ export FIREWALL_NAME=allow-tcp-rule-692
 
 # <<< Create an instance template >>>
 gcloud compute instance-templates create lb-backend-template \
-  --region=$REGION \
-  --network=default \
-  --subnet=default \
-  --tags=allow-health-check \
-  --machine-type=$MACHINE_TYPE \
-  --image-family=debian-11 \
-  --image-project=debian-cloud
+--region=$REGION \
+--network=default \
+--subnet=default \
+--tags=allow-health-check \
+--machine-type=$MACHINE_TYPE \
+--image-family=debian-11 \
+--image-project=debian-cloud
 
 # <<< Create a managed instance group based on the template >>>
 gcloud compute instance-groups managed create lb-backend-group \
-  --template=lb-backend-template  \
-  --size=2  \
-  --zone=$ZONE
+--template=lb-backend-template  \
+--size=2  \
+--zone=$ZONE
 
 # <<< Create a firewall rule named as Firewall rule to allow traffic (80/tcp) >>>
 gcloud compute firewall-rules create $FIREWALL_NAME \
-  --target-tags allow-health-check \
-  --allow tcp:80
+--target-tags allow-health-check \
+--allow tcp:80
 
 # <<< Create a health check >>>
 gcloud compute firewall-rules create fw-allow-health-check \
-  --network=default \
-  --action=allow \
-  --direction=ingress \
-  --source-ranges=130.211.0.0/22,35.191.0.0/16 \
-  --target-tags=allow-health-check \
-  --rules=tcp:80
+--network=default \
+--action=allow \
+--direction=ingress \
+--source-ranges=130.211.0.0/22,35.191.0.0/16 \
+--target-tags=allow-health-check \
+--rules=tcp:80
 
 gcloud compute firewall-rules create www-firewall-network-lb \
-  --target-tags allow-health-check --allow tcp:80
-
-  --allow tcp:80
+--target-tags allow-health-check 
+--allow tcp:80
 
 # <<< Create a backend service and add your instance group as the backend to the backend service group with named port (http:80) >>>
 gcloud compute backend-services create web-backend-service \
-  --protocol=HTTP \
-  --port-name=http \
-  --health-checks=fw-allow-health-check \
-  --global
+--protocol=HTTP \
+--port-name=http \
+--health-checks=fw-allow-health-check \
+--global
 
 gcloud compute backend-services add-backend web-backend-service \
-  --instance-group=lb-backend-group \
-  --instance-group-zone=$ZONE \
-  --global
+--instance-group=lb-backend-group \
+--instance-group-zone=$ZONE \
+--global
 
 # >>> Create a URL map, and target the HTTP proxy to route the incoming requests to the default backend service <<<
 gcloud compute url-maps create web-map-http \
-  --default-service web-backend-service
+--default-service web-backend-service
 
 # >>> Create a target HTTP proxy to route requests to your URL map <<<
 gcloud compute target-http-proxies create http-lb-proxy \
-    --url-map web-map-http
+--url-map web-map-http
 
 # >>> Create a forwarding rule <<<
 gcloud compute addresses create lb-ipv4-1 \
-  --ip-version=IPV4 \
-  --global
+--ip-version=IPV4 \
+--global
 
 gcloud compute forwarding-rules create http-content-rule \
-  --address=lb-ipv4-1\
-  --global \
-  --target-http-proxy=http-lb-proxy \
-  --ports=80
+--address=lb-ipv4-1\
+--global \
+--target-http-proxy=http-lb-proxy \
+--ports=80
--- solution_gsp313.sh
+++ solution_gsp313.sh
@@ -2,6 +2,7 @@
 
 export REGION="${ZONE%-*}"
 
+# stworzenie vpc
 gcloud compute networks create nucleus-vpc --subnet-mode=auto
 
 gcloud compute instances create $INSTANCE_NAME \
@@ -11,25 +12,26 @@ gcloud compute instances create $INSTANCE_NAME \
           --image-family debian-10  \
           --image-project debian-cloud 
 
- 
+#???
 gcloud container clusters create nucleus-backend \
 --num-nodes 1 \
 --network nucleus-vpc \
 --zone $ZONE
  
- 
+#???
 gcloud container clusters get-credentials nucleus-backend \
 --zone $ZONE
  
- 
+#???
 kubectl create deployment hello-server \
 --image=gcr.io/google-samples/hello-app:2.0
-  
+
+#???
 kubectl expose deployment hello-server \
 --type=LoadBalancer \
 --port $PORT
- 
-  
+
+
 cat << EOF > startup.sh
 #! /bin/bash
 apt-get update
@@ -38,36 +40,36 @@ service nginx start
 sed -i -- 's/nginx/Google Cloud Platform - '"\$HOSTNAME"'/' /var/www/html/index.nginx-debian.html
 EOF
 
- 
+ # <<< Create an instance template >>>
 gcloud compute instance-templates create web-server-template \
 --metadata-from-file startup-script=startup.sh \
 --network nucleus-vpc \
 --machine-type g1-small \
 --region $ZONE
  
- 
+#???
 gcloud compute target-pools create nginx-pool --region=$REGION
  
- 
+# <<< Create a managed instance group based on the template >>>
 gcloud compute instance-groups managed create web-server-group \
 --base-instance-name web-server \
 --size 2 \
 --template web-server-template \
 --region $REGION
  
- 
+# <<< Create a firewall rule named as Firewall rule to allow traffic (80/tcp) >>>
 gcloud compute firewall-rules create $FIREWALL_NAME \
 --allow tcp:80 \
 --network nucleus-vpc
  
- 
+# <<< Create a health check >>>
 gcloud compute http-health-checks create http-basic-check
 gcloud compute instance-groups managed \
 set-named-ports web-server-group \
 --named-ports http:80 \
 --region $REGION
  
- 
+# <<< Create a backend service and add your instance group as the backend to the backend service group with named port (http:80) >>>
 gcloud compute backend-services create web-server-backend \
 --protocol HTTP \
 --http-health-checks http-basic-check \
@@ -79,16 +81,16 @@ gcloud compute backend-services add-backend web-server-backend \
 --instance-group-region $REGION \
 --global
  
- 
+# >>> Create a URL map, and target the HTTP proxy to route the incoming requests to the default backend service <<<
 gcloud compute url-maps create web-server-map \
 --default-service web-server-backend
  
- 
+# >>> Create a target HTTP proxy to route requests to your URL map <<<
 gcloud compute target-http-proxies create http-lb-proxy \
 --url-map web-server-map
  
  
- 
+# >>> Create a forwarding rule <<<
 gcloud compute forwarding-rules create http-content-rule \
 --global \
 --target-http-proxy http-lb-proxy \
